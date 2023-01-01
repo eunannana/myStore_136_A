b7f7a18e10212f7e49825aabe7848946234f504d
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2023-01-01 08:59:38 +0700

    add editProduct function

--- src/main/java/WS/A/myStore/myStoreController.java
+++ src/main/java/WS/A/myStore/myStoreController.java
@@ -21,47 +21,83 @@ public class myStoreController {
     Myproduct data = new Myproduct();
     MyproductJpaController dataCtrl = new MyproductJpaController();
 
-     @RequestMapping("/getProduct/{id}")
-    public String getData(@PathVariable("id") int id){
+    @RequestMapping("/getProduct/{id}")
+    public String getData(@PathVariable("id") int id) {
 
-        try{
+        try {
             data = dataCtrl.findMyproduct(id);
-            return data.getProductName()+"<br>"+ data.getQty();
-        }
-        catch (Exception error){
-            return "Data is not found!!!";
+            return data.getProductName() + "<br>" + data.getQty();
+        } catch (Exception error) {
+            return "Product is not found!!!";
         }
     }
 
     @RequestMapping("/getProductAll")
-    public List<Myproduct> getDataAll()
-    {
+    public List<Myproduct> getDataAll() {
         return dataCtrl.findMyproductEntities();
     }
 
     @RequestMapping("/addProduct")
     public String addData(@RequestBody Myproduct theproduct) {
-        try{
+        try {
             dataCtrl.create(theproduct);
-            return "Successfully added data";
-        }
-        catch(Exception message){
+            return "Successfully added product";
+        } catch (Exception message) {
             return "Product is already exist";
         }
     }
 
-    
+    @RequestMapping("/editProduct/{id}")
+    public String editData(@PathVariable("id") int id, @RequestBody Myproduct theproduct) {
+        try {
+            data = dataCtrl.findMyproduct(id);
+            theproduct.setProductID(id);
 
-            
-    @RequestMapping("/deleteData/{id}")
-    public String deleteData(@PathVariable("id") int id){
-        try{
-            dataCtrl.destroy(id);
-            return "Successfully deleted data";
-        }
-        catch(NonexistentEntityException error){
-            return "Data is not found!!!";
+            if (theproduct.getProductID() != id) {
+                return "Product doesn't exist!";
+            } else if (theproduct.getProductName() == null) {
+                data = dataCtrl.findMyproduct(id);
+                theproduct.setProductName(data.getProductName());
+                dataCtrl.edit(theproduct);
+                Myproduct newproduct = new Myproduct();
+                newproduct = dataCtrl.findMyproduct(id);
+                return "Successfully edited :"
+                        + "\n - ProductID : " + data.getProductID()
+                        + "\n - ProductName : " + newproduct.getProductName()
+                        + "\n - Qty : " + newproduct.getQty();
+            } else if (theproduct.getQty() == null) {
+                data = dataCtrl.findMyproduct(id);
+                theproduct.setQty(data.getQty());
+                dataCtrl.edit(theproduct);
+                Myproduct newproduct = new Myproduct();
+                newproduct = dataCtrl.findMyproduct(id);
+                return "Successfully edited : "
+                        + "\n - ProductID : " + data.getProductID()
+                        + "\n - ProductName : " + newproduct.getProductName()
+                        + "\n - Qty : " + newproduct.getQty();
+            } else if (theproduct.getProductName() != null && theproduct.getQty() != null) {
+                dataCtrl.edit(theproduct);
+                Myproduct newproduct = new Myproduct();
+                newproduct = dataCtrl.findMyproduct(id);
+                return "Successfully edited : "
+                        + "\n - ProductID : " + data.getProductID()
+                        + "\n - ProductName : " + newproduct.getProductName()
+                        + "\n - Qty : " + newproduct.getQty();
+            } else {
+                return "ERROR!";
+            }
+        } catch (Exception e) {
+            return "Product is not found!";
         }
     }
 
+    @RequestMapping("/deleteData/{id}")
+    public String deleteData(@PathVariable("id") int id) {
+        try {
+            dataCtrl.destroy(id);
+            return "Successfully deleted product";
+        } catch (NonexistentEntityException error) {
+            return "Product is not found!!!";
+        }
+    }
 }
